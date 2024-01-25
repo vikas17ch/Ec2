7544ce78ef352ad48236de43c07efa60daba6bbe
vikas chava <87170539+vikas17ch@users.noreply.github.com>
2024-01-25 16:09:16 -0500

    Update Ec2.tf

--- Ec2.tf
+++ Ec2.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_instance" "vikas_ec2_instance" {
   ami                          = "ami-0005e0cfe09cc9050"
   instance_type                = "t2.micro"
